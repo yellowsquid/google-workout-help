a3140b2f909c6159583f0194ccf6f133ca5ff30b
Teapot <tty23@cam.ac.uk>
2020-01-30 12:40:47 +0000

    Add new sport

--- wear/src/main/java/com/example/testapp/MainActivity.java
+++ wear/src/main/java/com/example/testapp/MainActivity.java
@@ -1,13 +1,15 @@
 package com.example.testapp;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.wearable.activity.WearableActivity;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends WearableActivity {
 
-    private TextView mTextView;
+    private TextView activityText;
+    private TextView timeText;
     private ProgressBar pBar;
 
     @Override
@@ -15,11 +17,34 @@ public class MainActivity extends WearableActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = (TextView) findViewById(R.id.text);
+        activityText = (TextView) findViewById(R.id.exerciseName);
+        timeText = (TextView) findViewById(R.id.timeValue);
         pBar = (ProgressBar) findViewById(R.id.determinateBar);
 
-        pBar.setProgress(50);
+
         // Enables Always-on
         setAmbientEnabled();
+
+
+        newSport("StarJumps", 15000);
     }
+
+    public void newSport(String name, final long workoutLength){
+        activityText.setText(name);
+
+        new CountDownTimer(workoutLength, 100) {
+
+            public void onTick(long millisUntilFinished) {
+                double timeLeft = (double) millisUntilFinished / 1000;
+
+                //pBar.setProgress((int) ((L-millisUntilFinished)/L *100));
+                timeText.setText("" + String.format("%.1f", timeLeft));
+            }
+
+            public void onFinish() {
+                timeText.setText("done!");
+            }
+        }.start();
+    }
+
 }
