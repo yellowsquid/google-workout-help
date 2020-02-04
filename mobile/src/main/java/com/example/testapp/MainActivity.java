52e4cac710d9206cfbf728e277e70d930ce35e1a
Greg Brown <greg.brown.00@outlook.com>
2020-02-04 12:55:31 +0000

    Create intent for opening wait screen.

--- mobile/src/main/java/com/example/testapp/MainActivity.java
+++ mobile/src/main/java/com/example/testapp/MainActivity.java
@@ -1,21 +1,40 @@
 package com.example.testapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.testapp.shared.Circuit;
+import com.example.testapp.shared.Exercise;
+import com.example.testapp.shared.ExerciseType;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private Circuit circuit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Exercise> exercises = new ArrayList<>(5);
+        exercises.add(new Exercise(ExerciseType.BURPEES, 30));
+        exercises.add(new Exercise(ExerciseType.PUSHUPS, 30));
+        exercises.add(new Exercise(ExerciseType.RUSSIAN_TWISTS, 30));
+        exercises.add(new Exercise(ExerciseType.SQUATS, 30));
+        exercises.add(new Exercise(ExerciseType.STAR_JUMPS, 30));
+        circuit = new Circuit(exercises, 5);
     }
 
     public void onClick(View v) {
-        // TODO: send data to comms team.
-        ((TextView) findViewById(R.id.status)).setText("Sent");
+        Intent intent = new Intent(this, WaitingActivity.class);
+        intent.putExtra(WaitingActivity.ACCESS_TOKEN_ID, "tOkEn4T3sT1Ng");
+        intent.putExtra(WaitingActivity.CIRCUIT_ID, circuit);
+
+        startActivity(intent);
     }
 }
--- mobile/src/main/java/com/example/testapp/WaitingActivity.java
+++ mobile/src/main/java/com/example/testapp/WaitingActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WaitingActivity extends AppCompatActivity {
+    public final static String ACCESS_TOKEN_ID = "com.example.testapp.ACCESS_TOKEN_ID";
+    public final static String CIRCUIT_ID = "com.example.testapp.CIRCUIT_ID";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
