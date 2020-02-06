7f5647e0beb488dfa01dbe31f3e87f1dce7c84e5
OliverWales <oliverfwales@gmail.com>
2020-02-06 12:01:38 +0000

    Rename StartSignal to Signal, add message field

---
+++ shared/src/main/java/com/example/testapp/shared/Signal.java
@@ -0,0 +1,16 @@
+package com.example.testapp.shared;
+
+import java.io.Serializable;
+
+public class Signal implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private final String message;
+
+    Signal(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
--- shared/src/main/java/com/example/testapp/shared/StartSignal.java
+++
@@ -1,7 +0,0 @@
-package com.example.testapp.shared;
-
-import java.io.Serializable;
-
-public class StartSignal  implements Serializable {
-    private static final long serialVersionUID = 1L;
-}
