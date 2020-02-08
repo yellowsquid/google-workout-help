65277dde82ead5d8c10a487846b1b19457b6a222
OliverWales <oliverfwales@gmail.com>
2020-02-08 11:01:10 +0000

    Move data sending from main to waiting activity

--- mobile/src/main/java/com/example/testapp/MainActivity.java
+++ mobile/src/main/java/com/example/testapp/MainActivity.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
-    private Circuit circuit;
     public static final String CIRCUIT_MESSAGE_PATH = "/circuit_path_name";
     private static final String VOICE_TRANSCRIPTION_CAPABILITY_NAME = "voice_transcription";
     //public static final Context a = null;
@@ -42,23 +41,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        List<Exercise> exercises = new ArrayList<>(5);
-        exercises.add(new Exercise(ExerciseType.BURPEES, 30));
-        exercises.add(new Exercise(ExerciseType.PUSHUPS, 30));
-        exercises.add(new Exercise(ExerciseType.RUSSIAN_TWISTS, 30));
-        exercises.add(new Exercise(ExerciseType.SQUATS, 30));
-        exercises.add(new Exercise(ExerciseType.STAR_JUMPS, 30));
-        circuit = new Circuit(exercises, 5);
     }
 
     public void waitClicked(View v) throws ExecutionException, InterruptedException, IOException {
         Intent intent = new Intent(this, WaitingActivity.class);
-        Signal startSignal = new Signal("START");
-        byte[] startSignalBytes = Serializer.serialize(startSignal);
-        byte[] circuitBytes = Serializer.serialize(circuit);
-        sendDataToWatches(startSignalBytes);
-        sendDataToWatches(circuitBytes);
         startActivity(intent);
     }
 
@@ -66,31 +52,5 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, EditActivity.class);
 
         startActivity(intent);
-    }
-
-    private void sendDataToWatches(byte[] bytesData) throws ExecutionException, InterruptedException {
-        //LOGD(TAG, "Sendign start signal + circuit info");
-        HashSet<String> nodesToSend = (HashSet<String>) getNodes();
-        for (String nodeID : nodesToSend){
-            if (nodeID != null) {
-
-                Task<Integer> sendTask = Wearable.getMessageClient(getApplicationContext()).sendMessage(
-                        nodeID, CIRCUIT_MESSAGE_PATH, bytesData);
-                //sendTask.addOnSuccessListener(...);
-                //sendTask.addOnFailureListener(...);
-            } else {
-                // TODO: Unable to retrieve node with transcription capability
-            }
-        }
-    }
-
-    private Collection<String> getNodes() throws ExecutionException, InterruptedException {
-        HashSet<String> results = new HashSet<>();
-        List<Node> nodes =
-                Tasks.await(Wearable.getNodeClient(getApplicationContext()).getConnectedNodes());
-        for (Node node : nodes) {
-            results.add(node.getId());
-        }
-        return results;
     }
 }
--- mobile/src/main/java/com/example/testapp/WaitingActivity.java
+++ mobile/src/main/java/com/example/testapp/WaitingActivity.java
@@ -10,11 +10,26 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.testapp.shared.Circuit;
+import com.example.testapp.shared.Exercise;
+import com.example.testapp.shared.ExerciseType;
+import com.example.testapp.shared.Serializer;
+import com.example.testapp.shared.Signal;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.Tasks;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.Wearable;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import static com.example.testapp.MainActivity.CIRCUIT_MESSAGE_PATH;
 
 public class WaitingActivity extends AppCompatActivity {
+    private Circuit circuit;
     public final static String CIRCUIT_ID = "com.example.testapp.CIRCUIT_ID";
 
     @Override
@@ -45,11 +60,56 @@ public class WaitingActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setAdapter(adaptor);
+
+        // start activity
+        List<Exercise> exercises = new ArrayList<>(5);
+        exercises.add(new Exercise(ExerciseType.BURPEES, 30));
+        exercises.add(new Exercise(ExerciseType.PUSHUPS, 30));
+        exercises.add(new Exercise(ExerciseType.RUSSIAN_TWISTS, 30));
+        exercises.add(new Exercise(ExerciseType.SQUATS, 30));
+        exercises.add(new Exercise(ExerciseType.STAR_JUMPS, 30));
+        circuit = new Circuit(exercises, 5);
+    }
+
+    private Collection<String> getNodes() throws ExecutionException, InterruptedException {
+        HashSet<String> results = new HashSet<>();
+        List<Node> nodes =
+                Tasks.await(Wearable.getNodeClient(getApplicationContext()).getConnectedNodes());
+        for (Node node : nodes) {
+            results.add(node.getId());
+        }
+        return results;
+    }
+
+    private void sendDataToWatches(byte[] bytesData) throws ExecutionException, InterruptedException {
+        //LOGD(TAG, "Sendign start signal + circuit info");
+        HashSet<String> nodesToSend = (HashSet<String>) getNodes();
+        for (String nodeID : nodesToSend){
+            if (nodeID != null) {
+
+                Task<Integer> sendTask = Wearable.getMessageClient(getApplicationContext()).sendMessage(
+                        nodeID, CIRCUIT_MESSAGE_PATH, bytesData);
+                //sendTask.addOnSuccessListener(...);
+                //sendTask.addOnFailureListener(...);
+            } else {
+                // TODO: Unable to retrieve node with transcription capability
+            }
+        }
     }
 
     public void startClicked(View v) {
-        Intent intent = new Intent(this, DuringActivity.class);
-
-        startActivity(intent);
+        Signal startSignal = new Signal("START");
+        try {
+            byte[] startSignalBytes = com.example.testapp.shared.Serializer.serialize(startSignal);
+            byte[] circuitBytes = Serializer.serialize(circuit);
+            sendDataToWatches(startSignalBytes);
+            sendDataToWatches(circuitBytes);
+            Intent intent = new Intent(this, DuringActivity.class);
+            startActivity(intent);
+        } catch (IOException e) {
+            throw new RuntimeException("Serialization failed");
+        } catch (InterruptedException |  ExecutionException e) {
+            // ignore
+        }
     }
 }
