cf1d54f4fb0a8f0e9cc56089a8cdc217b015cbc8
OliverWales <oliverfwales@gmail.com>
2020-02-08 11:38:58 +0000

    Change Signal class to enum

--- shared/src/main/java/com/example/testapp/shared/Signal.java
+++ shared/src/main/java/com/example/testapp/shared/Signal.java
@@ -2,16 +2,7 @@ package com.example.testapp.shared;
 
 import java.io.Serializable;
 
-// TODO: Investigate switching to enum.
-public class Signal implements Serializable {
-    private static final long serialVersionUID = 1L;
-    private final String message;
-
-    public Signal(String message) {
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+public enum Signal implements Serializable {
+    START;
+    private static final long serialVersionUID = 2L;
 }
