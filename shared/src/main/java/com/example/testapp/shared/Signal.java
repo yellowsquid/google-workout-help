1593b5823493af6339f2f137a98f6515e8e290fd
OliverWales <oliverfwales@gmail.com>
2020-02-11 11:18:27 +0000

    Add STOP, PAUSE and RESUME signals to Signal enum

--- shared/src/main/java/com/example/testapp/shared/Signal.java
+++ shared/src/main/java/com/example/testapp/shared/Signal.java
@@ -3,6 +3,9 @@ package com.example.testapp.shared;
 import java.io.Serializable;
 
 public enum Signal implements Serializable {
-    START;
+    START,
+    STOP,
+    PAUSE,
+    RESUME;
     private static final long serialVersionUID = 2L;
 }
