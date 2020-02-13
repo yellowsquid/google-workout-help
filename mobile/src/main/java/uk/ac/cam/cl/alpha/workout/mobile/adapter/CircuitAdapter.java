7fb9b7e5f8190ca36e221db5b1c1b83e056b5c54
Greg Brown <greg.brown.00@outlook.com>
2020-02-13 13:36:19 +0000

    Make circuit view holder update view from circuit.

--- mobile/src/main/java/uk/ac/cam/cl/alpha/workout/mobile/adapter/CircuitAdapter.java
+++ mobile/src/main/java/uk/ac/cam/cl/alpha/workout/mobile/adapter/CircuitAdapter.java
@@ -1,5 +1,6 @@
 package uk.ac.cam.cl.alpha.workout.mobile.adapter;
 
+import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +15,8 @@ import java.util.List;
 import uk.ac.cam.cl.alpha.workout.R;
 import uk.ac.cam.cl.alpha.workout.shared.Circuit;
 
-public class CircuitAdapter extends RecyclerView.Adapter {
-
-    List<Circuit> circuits;
+public class CircuitAdapter extends RecyclerView.Adapter<CircuitAdapter.CircuitViewHolder> {
+    private List<Circuit> circuits;
 
     public CircuitAdapter(List<? extends Circuit> circuits) {
         this.circuits = new ArrayList<>();
@@ -25,21 +25,16 @@ public class CircuitAdapter extends RecyclerView.Adapter {
 
     @NonNull
     @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public CircuitViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.circuit_select_layout, parent, false);
         return new CircuitViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull CircuitViewHolder holder, int position) {
         Circuit circuit = circuits.get(position);
-        CircuitViewHolder circuitViewHolder = (CircuitViewHolder) holder;
-
-        circuitViewHolder.nameTextView.setText(circuit.getName());
-        circuitViewHolder.durationTextView.setText(String.format("Time: %d secs", circuit.getTotalDurationSecs()));
-        circuitViewHolder.restTextView.setText(String.format("Rest: %d secs", 1, circuit.getTotalRestSecs()));
-        circuitViewHolder.lapsTextView.setText(String.format("Laps: %d", circuit.getLaps()));
+        holder.setCircuit(circuit);
     }
 
     @Override
@@ -47,20 +42,31 @@ public class CircuitAdapter extends RecyclerView.Adapter {
         return circuits.size();
     }
 
-    public static class CircuitViewHolder extends RecyclerView.ViewHolder {
+    static class CircuitViewHolder extends RecyclerView.ViewHolder {
         // FIXME: encapsulation non-existent
-        public TextView nameTextView;
-        public TextView durationTextView;
-        public TextView restTextView;
-        public TextView lapsTextView;
+        private TextView nameTextView;
+        private TextView durationTextView;
+        private TextView restTextView;
+        private TextView lapsTextView;
 
-        public CircuitViewHolder(View view) {
+        CircuitViewHolder(View view) {
             super(view);
 
             nameTextView = view.findViewById(R.id.circuitName);
             durationTextView = view.findViewById(R.id.circuitDuration);
             restTextView = view.findViewById(R.id.circuitRestTime);
             lapsTextView = view.findViewById(R.id.circuitLaps);
+        }
+
+        void setCircuit(Circuit circuit) {
+            Resources resources = nameTextView.getResources();
+            int duration = circuit.getTotalDurationSecs();
+            int rest = circuit.getTotalRestSecs();
+            int laps = circuit.getLaps();
+            nameTextView.setText(circuit.getName());
+            durationTextView.setText(resources.getString(R.string.duration_is, duration));
+            restTextView.setText(resources.getString(R.string.rest_is, rest));
+            lapsTextView.setText(resources.getString(R.string.laps_is, laps));
         }
     }
 }
