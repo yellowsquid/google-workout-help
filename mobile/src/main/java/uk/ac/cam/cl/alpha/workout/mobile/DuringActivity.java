aea16c6c37a393b4789509ba4d468b73d48299e0
Brodie <askerbrodie@gmail.com>
2020-02-18 12:00:25 +0000

    Fix crash when pressing back button on during screen

--- mobile/src/main/java/uk/ac/cam/cl/alpha/workout/mobile/DuringActivity.java
+++ mobile/src/main/java/uk/ac/cam/cl/alpha/workout/mobile/DuringActivity.java
@@ -1,6 +1,7 @@
 package uk.ac.cam.cl.alpha.workout.mobile;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +13,17 @@ public class DuringActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_during);
+    }
+
+    // Override method in order to properly finish() this activity when the software back button is
+    // pressed. This prevents a crash caused by onCreate() getting called again and trying to
+    // populate things with a null circuit.
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
     }
 }
