cf771e1c8ad4b3a0082c7d04a24df783daf6eebf
Teapot <tty23@cam.ac.uk>
2020-02-29 21:07:51 +0000

    fixed hours displaying larger than they are

--- shared/src/main/java/uk/ac/cam/cl/alpha/workout/shared/TimeFormatter.java
+++ shared/src/main/java/uk/ac/cam/cl/alpha/workout/shared/TimeFormatter.java
@@ -5,18 +5,21 @@ import java.util.Date;
 import java.util.Locale;
 
 public final class TimeFormatter {
+
+    public static final int TIMECONST = 60;
+
     private TimeFormatter(){}
 
     public static String format(int seconds) {
-        SimpleDateFormat hoursMinutesSeconds = new SimpleDateFormat("HH:mm:ss", Locale.getDefault());
-        SimpleDateFormat minutesSeconds = new SimpleDateFormat("mm:ss", Locale.getDefault());
         String formatted;
 
-        if (seconds > Constants.SECONDS_IN_HOUR) {
-            formatted = hoursMinutesSeconds.format(new Date(seconds * Constants.MILLISECONDS_IN_SECOND));
-        } else {
-            formatted = minutesSeconds.format(new Date(seconds * Constants.MILLISECONDS_IN_SECOND));
-        }
+        int sec = seconds % TIMECONST;
+        int hour = seconds / TIMECONST;
+        int min = hour % TIMECONST;
+        hour /= TIMECONST;
+
+        formatted = seconds >= Constants.SECONDS_IN_HOUR ? String.format("%d:%02d:%02d", hour, min, sec)
+                : String.format("%02d:%02d", min, sec);
 
         return formatted;
     }
